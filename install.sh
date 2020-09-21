ee4bd2ba66b5d75cd4741bb387fb05b5c3463abd
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-21 16:08:56 +0200

    Fix path shit fuck

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+
 # Install, provide as first agument, the directory where packages are listed
 
 sudo apt install python3 # Just make sure we have python3
 sudo apt install software-properties-common # Need that add-apt-repository
-python3 ./apt-installer.py $(cwd)/$1
+python3 $DIR/apt-installer.py $(abspath $1)
